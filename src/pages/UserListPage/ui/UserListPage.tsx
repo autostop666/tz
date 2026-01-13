4b303263fee5e7b68afe53cebee9909d4022ccd2
autostop666 <krasiviy.radnoi@gmail.com>
2026-01-13 12:20:10 +0500

    added styles and pagination in table

--- src/pages/UserListPage/ui/UserListPage.tsx
+++ src/pages/UserListPage/ui/UserListPage.tsx
@@ -1,26 +1,25 @@
-import { Col, Row, Table } from "antd"
-import styles from './UserListPage.module.less'
-import { Columns } from "../module/const/const"
-import { useEffect, useState } from "react"
-import { UsersListType } from "../../UserPage/types/types"
-import { getUsers } from "../service/service"
+import { Col, Row, Table } from "antd";
+import styles from "./UserListPage.module.less";
+import { Columns } from "../module/const/const";
+import { useEffect, useState } from "react";
+import { UsersListType } from "../../UserPage/types/types";
+import { getUsers } from "../service/service";
 
-const UserListPage = () =>{
-const [users, setUsers] = useState<UsersListType[]>([])
-useEffect(()=>{
-  getUsers().then((res) => setUsers(res.data))
-}, [])
+const UserListPage = () => {
+  const [users, setUsers] = useState<UsersListType[]>([]);
+  useEffect(() => {
+    getUsers().then((res) => setUsers(res.data));
+  }, []);
 
-
-  return(
+  return (
     <Row className={styles.center}>
       <Col span={24}>
         <h1>UserListPage</h1>
       </Col>
       <Col span={24}>
-        <Table columns={Columns} dataSource={users}/>
+        <Table columns={Columns} className={styles.table} dataSource={users} />
       </Col>
     </Row>
-  )
-}
-export default UserListPage
+  );
+};
+export default UserListPage;
